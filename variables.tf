9e5edc227581c60230c75ea01a8e6015496105fc
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-05-28 14:39:47 +0530

    adds terraform files

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# main creds for Azure connection
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+}
+
+variable "azure_client_id" {
+  description = "Azure Client ID"
+}
+
+variable "azure_client_secret" {
+  description = "Azure Client Secret"
+}
+
+variable "azure_tenant_id" {
+  description = "Azure Tenant ID"
+}
