9ad3a527b9163360ab177b914cf7e8c0102b63f1
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-05-30 11:38:14 +0530

    changes vm code

--- vm.tf
+++ vm.tf
@@ -12,34 +12,46 @@ resource "azurerm_virtual_machine" "demo_vm_tf" {
   network_interface_ids = ["${var.nic_id}"]
   vm_size               = "Standard_B1s"
 
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
   storage_os_disk {
-    name              = "osdisk1"
+    name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
-    version   = "latest"
+  # Optional data disks
+  storage_data_disk {
+    name              = "datadisk_new"
+    managed_disk_type = "Standard_LRS"
+    create_option     = "Empty"
+    lun               = 0
+    disk_size_gb      = "80"
   }
 
   os_profile {
-    computer_name  = "demovm"
-    admin_username = "azureuser"
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "thisis!ag00dpassword"
   }
 
   os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-        path     = "/home/azureuser/.ssh/authorized_keys"
-        key_data = "${var.ssh_public_key}"
-    }
+    disable_password_authentication = false
   }
 
   tags {
-      environment = "Terraform Demo"
+    environment = "Terraform Demo"
   }
 }
